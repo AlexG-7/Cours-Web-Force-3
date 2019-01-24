206eaffaf3d2e2af9736d735a1dd85ef3f3c7f5a
lionel aflalo (dldroopy) <lionel.aflalo@gmail.com>
2019-01-24 09:50:25 +0100

    .

--- AJAX/boutique/admin/gestion_commandes.php
+++ AJAX/boutique/admin/gestion_commandes.php
@@ -23,58 +23,67 @@ require_once('../inc/header.inc.php');
 
 <table class="table table-dark table-fluid">
 
-    <tr>
-        <th>ID commande</th>
-        <th>Membre</th>
-        <th>Montant</th>
-        <th>Date</th>
-        <th colspan="2">État</th>
-        <th>Détails</th>
-    </tr>
+  <tr>
+    <th>ID commande</th>
+    <th>Membre</th>
+    <th>Montant</th>
+    <th>Date</th>
+    <th colspan="2">État</th>
+    <th>Détails</th>
+  </tr>
 
-    <?php foreach($commandes as $commande) :
+  <?php foreach($commandes as $commande) :
         $date = date_create($commande['date_enregistrement']);
         $montant = number_format($commande['montant'], 2, ',', ' ');
         ?>
 
-    <tr>
-        <td><?= $commande['id_commande'] ?></td>
-        <td><?= $commande['membre'] ?></td>
-        <td><?= $montant ?> €</td>
-        <td><?= date_format($date, 'd/m/Y H:i:s') ?></td>
-        <td><span id="etat-commande-<?= $commande['id_commande']?>"><?= ucfirst($commande['etat']) ?></span></td>
-        <td><button class="btn btn-primary maj" data-id="<?= $commande['id_commande']?>" <?= ($commande['etat'] == 'livré') ? 'disabled' : ''?>>MàJ</button></td>
-        <td><button class="btn btn-primary see" data-id="<?= $commande['id_commande']?>"><i class="fas fa-eye"></i></button></td>
-    </tr>
+  <tr>
+    <td>
+      <?= $commande['id_commande'] ?>
+    </td>
+    <td>
+      <?= $commande['membre'] ?>
+    </td>
+    <td>
+      <?= $montant ?> €</td>
+    <td>
+      <?= date_format($date, 'd/m/Y H:i:s') ?>
+    </td>
+    <td><span id="etat-commande-<?= $commande['id_commande']?>">
+        <?= ucfirst($commande['etat']) ?></span></td>
+    <td><button class="btn btn-primary maj" data-id="<?= $commande['id_commande']?>" <?=($commande['etat']=='livré' ) ? 'disabled' : '' ?>>MàJ</button></td>
+    <td><button class="btn btn-primary see" data-id="<?= $commande['id_commande']?>"><i class="fas fa-eye"></i></button></td>
+  </tr>
 
-    <?php endforeach?>
+  <?php endforeach?>
 
 </table>
 
 <div id="detail"></div>
 
 <div class="modal" tabindex="-1" role="dialog" id="myModal">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-        <div class="modal-header">
-            <h4 class="modal-title text-center" id="modal-title">Fiche produit de <span id="nom-produit"></span></h4>
-            <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title text-center" id="modal-title">Fiche produit de <span id="nom-produit"></span></h4>
+        <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button> -->
-        </div>
-        <div class="modal-body">
-            <img src="" alt="" class="img-responsive img-boutique" id="img-prod"><hr>
-			<p class="text-center">Catégorie : <span id="cat-prod"></span></p>
-			<p class="text-center">Couleur : <span id="col-prod"></span></p>
-			<p class="text-center">Taille : <span id="taille-prod"></span></p>
-			<p class="text-center">Description : <span id="desc-prod"></span></p>
-			<p class="text-center">Prix : <span id="prix-prod"></span> €</p>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-primary" data-dismiss="modal">Fermer</button>
-        </div>
-        </div>
+      </div>
+      <div class="modal-body">
+        <img src="" alt="" class="img-responsive img-boutique" id="img-prod">
+        <hr>
+        <p class="text-center">Catégorie : <span id="cat-prod"></span></p>
+        <p class="text-center">Couleur : <span id="col-prod"></span></p>
+        <p class="text-center">Taille : <span id="taille-prod"></span></p>
+        <p class="text-center">Description : <span id="desc-prod"></span></p>
+        <p class="text-center">Prix : <span id="prix-prod"></span> €</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Fermer</button>
+      </div>
     </div>
+  </div>
 </div>
 
 <?php
