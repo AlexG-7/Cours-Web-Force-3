6b0f8f5aac4665354f0e6fdf09aaed9a2acef75b
lionel aflalo (dldroopy) <lionel.aflalo@gmail.com>
2019-02-05 14:01:41 +0100

    symfony

--- SYMFONY/symfony-intro/src/Controller/IndexController.php
+++ SYMFONY/symfony-intro/src/Controller/IndexController.php
@@ -26,33 +26,38 @@ class IndexController extends AbstractController
     }
 
     /**
-     * Une route avec une aprtie variable (entre accolades)
-     * Le $qui en paramètre de la méthode contient la valeur de cette partie variable
+     * Une route avec une partie variable (entre accolades)
+     * le $qui en paramètre de la méthode contient la valeur
+     * de cette partie variable
      *
      * @Route("/bonjour/{qui}")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function bonjour($qui)
     {
-        return $this->render('index/bonjour.html.twig',
-            // Le tableau en 2ème paramètre de la méthode render()
+        return $this->render(
+            'index/bonjour.html.twig',
+            // le tableau en 2e paramètre de la méthode render()
             // permet de passer des variables au template :
-            // le nom de la variable est la clé dans ce tableau
+            // Le nom de la variable est la clé dans ce tableau
             [
-                'qui' => $qui
+                'qui' => $qui,
             ]
         );
     }
 
     /**
-     * Valeur par défaut pour la partie variable :
-     * la route matche /salut/UnNom : $qui vaut "UnNom"
-     * et matche aussi /salut :$qui vaut "toi"
+     * valeur par défaut pour la partie variable :
+     * la route matche /salut/UnNom : $qui vaut "UnNom",
+     * et matche aussi /salut : $qui vaut "toi"
      *
      * @Route("/salut/{qui}", defaults={"qui": "toi"})
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function salut($qui)
     {
-        return $this->render('index/salut.html.twig',
+        return $this->render(
+            'index/salut.html.twig',
             [
                 'qui' => $qui
             ]
@@ -60,19 +65,20 @@ class IndexController extends AbstractController
     }
 
     /**
-     * La route matche /coucou/UnPrénom et /coucou/UnPrénom-UnNom
+     * La route matche /coucou/Julien et /coucou/Julien-Anest
      *
-     * @Route("/coucou/{firstname}-{lastname}", defaults={"lastname": ""})
+     * @Route("/coucou/{firstname}/{lastname}", defaults={"lastname": ""})
      */
     public function coucou($firstname, $lastname)
     {
         $name = $firstname;
 
-        if ($lastname != ''){
+        if ($lastname != '') {
             $name .= ' ' . $lastname;
         }
 
-        return $this->render('index/coucou.html.twig',
+        return $this->render(
+            'index/coucou.html.twig',
             [
                 'name' => $name
             ]
@@ -80,14 +86,17 @@ class IndexController extends AbstractController
     }
 
     /**
-     * Heure dit forcément être un nombre (\d+ en expression régulière)
+     * heure doit forcément être un nombre (\d+ en expression régulière)
      *
      * @Route("/bonsoir/{heure}", requirements={"heure": "\d+"})
      *
+     * @param $heure
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function bonsoir($heure)
     {
-        return $this->render('index/bonsoir.html.twig',
+        return $this->render(
+            'index/bonsoir.html.twig',
             [
                 'heure' => $heure
             ]
--- SYMFONY/symfony-intro/src/Controller/TwigController.php
+++ SYMFONY/symfony-intro/src/Controller/TwigController.php
@@ -9,21 +9,26 @@ use Symfony\Component\Routing\Annotation\Route;
  * Class TwigController
  * @package App\Controller
  *
- * Préfixe de route sur toutes les routes définies dans la classe
+ * préfixe de route pour toutes les routes définies dans la classe
  * @Route("/twig")
  */
 class TwigController extends AbstractController
 {
     /**
-     *
-     * Avec le préfixe de route de la classe, l'url de cette route est /twig ou /twig/
+     * avec le préfixe de route de la classe,
+     * l'url de cette route est /twig ou /twig/
      * @Route("/")
      */
     public function index()
     {
-        return $this->render('twig/index.html.twig',
-            [
+        $date = new \DateTime();
 
+        echo $date->format('d/m/y');
+
+        return $this->render(
+            'twig/index.html.twig',
+            [
+                'auj' => new \DateTime()
             ]
         );
     }
